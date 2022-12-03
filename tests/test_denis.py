a7f59751aaabf983ddff2ac551a994e8f3673210
Maxim <maze.zte@gmail.com>
2022-12-03 16:02:45 +1000

    Update test_denis formate

--- tests/test_denis.py
+++ tests/test_denis.py
@@ -3,17 +3,25 @@ import unittest
 import algorithm_2
 
 
-class U(unittest.TestCase):
+class RemoveUnreachableSymbols(unittest.TestCase):
     def setUp(self):
         self.needed_grammar = Grammar({"E", "T", "F"},
                                       {"a", "+"},
-                                      {"E": ["E+T", "TF"], "T": ["T", "F"], "F": ["F", "a"]},
+                                      {
+                                          "E": ["E+T", "TF"],
+                                          "T": ["T", "F"],
+                                          "F": ["F", "a"]
+                                      },
                                       "E")
 
     def test_1_no_changes(self):
         tested_grammar = Grammar({"E", "T", "F"},
                                  {"a", "+"},
-                                 {"E": ["E+T", "TF"], "T": ["T", "F"], "F": ["F", "a"]},
+                                 {
+                                     "E": ["E+T", "TF"],
+                                     "T": ["T", "F"],
+                                     "F": ["F", "a"]
+                                 },
                                  "E")
 
         self.assertEqual(algorithm_2.remove_unreachable_symbols(tested_grammar), self.needed_grammar)
@@ -21,7 +29,12 @@ class U(unittest.TestCase):
     def test_2_unreachable_non_terminals(self):
         g1 = Grammar({"E", "T", "J", "F"},
                      {"a", "+"},
-                     {"E": ["E+T", "TF"], "T": ["T", "F"], "F": ["F", "a"], "J":["J"]},
+                     {
+                         "E": ["E+T", "TF"],
+                         "T": ["T", "F"],
+                         "F": ["F", "a"],
+                         "J": ["J"]
+                     },
                      "E")
 
         self.assertEqual(algorithm_2.remove_unreachable_symbols(g1), self.needed_grammar)
@@ -29,7 +42,11 @@ class U(unittest.TestCase):
     def test_3_unreachable_terminals(self):
         g2 = Grammar({"E", "T", "F"},
                      {"a", "+", "d"},
-                     {"E": ["E"], "T": ["T", "F"], "F": ["F", "a"]},
+                     {
+                         "E": ["E"],
+                         "T": ["T", "F"],
+                         "F": ["F", "a"]
+                     },
                      "E")
 
         g_needed2 = Grammar({"E"},
